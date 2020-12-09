21a4e4977ad3182248c1c5e1a7a07b51990b21ef
brianmituka <mitukab@outlook.com>
2020-12-09 22:45:53 +0300

    CORE-4265 return hex encoded cipher

--- lib/crypto.dart
+++ lib/crypto.dart
@@ -5,6 +5,7 @@ import 'package:pointycastle/asymmetric/pkcs1.dart';
 import 'package:pointycastle/asymmetric/rsa.dart';
 import 'package:pointycastle/key_generators/rsa_key_generator.dart';
 import 'package:pointycastle/pointycastle.dart';
+import 'package:hex/hex.dart';
 // import "package:pointycastle/export.dart";
 
 //TODO Add key signature verification
@@ -12,8 +13,8 @@ import 'package:pointycastle/pointycastle.dart';
 AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey> generateRSAKeyPair(
     SecureRandom secureRandom,
     {int bitLength = 4096}) {
-  final rsaParams =
-      RSAKeyGeneratorParameters(BigInt.parse('65537', radix: 16), bitLength, 64);
+  final rsaParams = RSAKeyGeneratorParameters(
+      BigInt.parse('65537', radix: 16), bitLength, 64);
   final keyGen = RSAKeyGenerator()
     ..init(ParametersWithRandom(rsaParams, secureRandom));
   final keyPair = keyGen.generateKeyPair();
@@ -27,6 +28,10 @@ String encrypt(RSAPublicKey publicKey, String dataToEncrypt) {
   final encryptor = PKCS1Encoding(RSAEngine())..init(true, publicKeyParams);
   var ciphertext =
       encryptor.process(Uint8List.fromList(dataToEncrypt.codeUnits));
+  var hexcipher = HEX.encode(ciphertext);
+  print('CIpher textoriji $ciphertext');
+  print('Hex:: $hexcipher');
+
   return String.fromCharCodes(ciphertext);
 }
 
